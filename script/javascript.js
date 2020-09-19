56837dd3086c2674c6d3e8b3a2583f8b17ecac98
ejunior95 <e.junior95@gmail.com>
2020-09-19 14:44:17 -0300

    [UPDATE] -> Atualizações na NavBar

---
+++ script/javascript.js
@@ -0,0 +1,8 @@
+
+// Função para abrir outra landing page pela NavBar
+
+function AbrirSecao(secao) {
+  window.open("" + secao + "", "_parent");
+}
+
+// Função para abrir outra landing page pela NavBar
