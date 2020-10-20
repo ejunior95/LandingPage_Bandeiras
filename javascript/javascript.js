621e314cf13cbd107351d919e3d48e53a1d217c0
ejunior95 <e.junior95@gmail.com>
2020-10-20 17:09:28 -0300

    [UPDATE]-> Alterações para responsividade

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -6,3 +6,18 @@ function AbrirSecao(secao) {
 }
 
 // Função para abrir outra landing page pela NavBar
+
+//Função para descer o menureduzido
+var verificadorMenu = false
+function DescerMenu() {
+  var x = document.getElementById("menureduzidoID");
+  if (verificadorMenu===false) {
+    x.classList.add('active');
+    verificadorMenu = true;
+  } else {
+    x.classList.remove('active');
+    verificadorMenu = false;
+  }
+}
+
+//Função para descer o menureduzido
